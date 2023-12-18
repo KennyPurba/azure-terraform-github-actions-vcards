10a960b1e93cb7e8a199acb298d9028fb1dc1f59
Muhammad Ghozy Nurcahyadi AGIT <75375413+mghozynAGIT@users.noreply.github.com>
2023-12-18 10:37:20 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -53,6 +53,33 @@ resource "azurerm_linux_web_app" "webapp" {
 
 }
 
+
+# Create the Linux App Service Plan
+resource "azurerm_service_plan" "appserviceplan2" {
+  name                = "webapp-demo2-${var.prefix_environment}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+# Create the web app, pass in the App Service Plan ID
+resource "azurerm_linux_web_app" "webapp" {
+  name                = "webapp-demo2-${var.prefix_environment}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.appserviceplan2.id
+  https_only          = true
+  site_config {
+    minimum_tls_version = "1.2"
+    application_stack {
+      docker_registry_url = "https://docker.io"
+      docker_image_name   = "nginx:latest"
+    }
+  }
+
+}
+
 # Create Storage Account in Resource Group
 resource "azurerm_storage_account" "attachmentstorage" {
   name                     = "attachmentstorage${lower(var.prefix_environment)}"
