a6ba2a5ab17ef0642026ff09cf6062e6d37d6596
Muhammad Ghozy Nurcahyadi AGIT <75375413+mghozynAGIT@users.noreply.github.com>
2023-12-18 10:38:09 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -64,7 +64,7 @@ resource "azurerm_service_plan" "appserviceplan2" {
 }
 
 # Create the web app, pass in the App Service Plan ID
-resource "azurerm_linux_web_app" "webapp" {
+resource "azurerm_linux_web_app" "webapp2" {
   name                = "webapp-demo2-${var.prefix_environment}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
